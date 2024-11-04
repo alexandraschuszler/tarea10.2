6f7bdb624210b6452fea44ce5e355e4dd00e0ea5
alexandra_schuszler <132113346+alexandraschuszler@users.noreply.github.com>
2024-11-03 23:48:21 -0300

    Delete Desafío LocalStorage directory

--- Desafío LocalStorage/js/data.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let dato = localStorage.getItem("datoGuardado")
-    if(dato){
-        document.getElementById("data").textContent = dato;
-    }else{
-        docuement.getElementById("data").textContent = "No hay datos";
-    }   
-})
--- Desafío LocalStorage/js/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("buttonText").addEventListener("click", function(){
-        let dato = document.getElementById("inputText").value;
-        if(dato){
-            localStorage.setItem("datoGuardado", dato);
-            window.location.href = "data.html";
-        }else{
-            alert("Por favor ingrese un dato")
-        }
-})
